2f72894acb8a5a5ffb196bdd529fbd93473a4a55
Nikhilthadani <63336772+Nikhilthadani@users.noreply.github.com>
2025-06-29 17:33:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer();
 server.on("request", (req, res) => {
-  res.end("SERVER READY!!!!!!!!!!!!!");
+  res.end("SERVER READY UPDATED");
 });
 
 server.listen(5000);
