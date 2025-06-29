bfd4bbbc96cfbf291a6145795bd5bbc7aefbd2d2
Nikhil Thadani <nikhilthadani112256@gmail.com>
2025-06-29 17:44:56 +0530

    a

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer();
 server.on("request", (req, res) => {
-  res.end("SERVER OK");
+  res.end("SERVER OK!");
 });
 
 server.listen(5000);
