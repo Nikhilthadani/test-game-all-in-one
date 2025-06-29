d8694e51565228df7ebf37ba87f2a4d9fc37a694
Nikhil Thadani <nikhilthadani112256@gmail.com>
2025-06-29 23:02:10 +0530

    added back http

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,8 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // ✅ allow all
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  if (req.method === "OPTIONS") {
-    res.writeHead(204);
-    return res.end();
-  }
-
-  res.writeHead(200, { "Content-Type": "text/plain" });
+const server = http.createServer();
+server.on("request", (req, res) => {
   res.end("SERVER OK!");
 });
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, "0.0.0.0");
