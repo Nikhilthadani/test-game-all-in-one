3118404478426b35ab70991df26f126c9d8c50d5
Nikhil Thadani <nikhilthadani112256@gmail.com>
2025-06-29 23:43:24 +0530

    added express

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,20 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer();
-server.on("request", (req, res) => {
-  res.write("OKOKOKOKOK");
-  res.end("SERVER OK!");
+const app = express();
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  return res.send("Health OK!");
 });
+
+app.get("/api/user", (req, res) => {
+  return res.json({ id: 1, name: "Nikhil", email: "n@test.com" });
+});
+app.get("/", (req, res) => {
+  return res.send("<h1>Welcome to my app</h1>");
+});
+
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, "0.0.0.0", () => {
-  console.log("SERVER OPEN");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("App open at port: ", PORT);
 });
